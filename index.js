435475da2a2b5c75944385bdcf2f06e6856628ba
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2017-10-11 11:10:30 +0200

    onClick method @ timer tray class

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const TimerTray = require('./app/timer_tray');
 const { app, BrowserWindow, Tray } = electron;
 
 let mainWindow;
-let tray;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
@@ -19,27 +18,6 @@ app.on('ready', () => {
 
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
-  tray = new TimerTray(iconPath);
+  new TimerTray(iconPath, mainWindow);
 
-  tray.on('click', (event, bounds) => {
-    // click event bounds
-    const { x, y } = bounds;
-
-    // window height and width
-    const { height, width } = mainWindow.getBounds();
-
-
-    if(mainWindow.isVisible()){
-      mainWindow.hide();
-    } else {
-      const yPosition = process.platform === 'darwin' ? y : y - height;
-      mainWindow.setBounds({
-        x: x - width / 2,
-        y: yPosition,
-        height,
-        width
-      });
-      mainWindow.show();
-    }
-  });
 });
